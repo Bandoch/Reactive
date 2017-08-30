4655c84578ec4db1e972d11e2376ac6b843d98b3
Bandoch <31252131+Bandoch@users.noreply.github.com>
2017-08-30 13:56:32 -0300

    ValidateCnpj removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { ValidateCnpj } from './validators/cnpj.validator';
 import {NgxBrValidators} from "ngx-br/ngx-br-validators";
 
 @Component({
